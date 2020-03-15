27f19dce252066d03ed145a4a7d7a9ef80b8adc9
mkspwr <manojsh@gmail.com>
2020-03-15 14:29:46 -0700

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: "mongodb://localhost:27017/backendAPIdb"
+    url: "mongodb://kakkoo:Rm2aHeXN1DP1mSO4etyp0uEjMMbB1YcFT3ceYnv3P5OJRGYqe28thnznr6b3sN6scE5MQCGrZNhr29ZS12kcGA==@kakkoo.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@kakkoo@"
 };
